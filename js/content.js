7babf55a42730a333a9d4e7004830c45b5d0eda7
Danilo Aparecido Namitala <danilonamitala@yahoo.com.br>
2021-01-15 21:02:22 -0300

    html updated

--- js/content.js
+++ js/content.js
@@ -1,14 +1,14 @@
-chrome.runtime.onMessage.addListener(                                       //evento que monitora a chegada de uma mensagem
+chrome.runtime.onMessage.addListener(
     function(message, sender, sendResponse) {
-        var container = document.getElementsByClassName('i5ly3 _2NwAr')[1]; //seleciona a div que contem a coluna de contatos
-        if(message.type == "hide"){                                         //se a menssagem traz o comando hide a div é oculta
+        var container = document.getElementsByClassName('i5ly3 _2NwAr')[1];
+        if(message.type == "hide"){
             container.style.display = 'none';
             sendResponse("hidden");
-        }else if(message.type == "show"){                                   //se a menssagem traz o comando hide a div é exibida
+        }else if(message.type == "show"){
             container.style.display = '';
             sendResponse("visible");
-        }else if(message.type == "state"){                                  //o comando state da uma resposta com o estado da div para que
-            if(container.style.display == 'none')                           //o switch seja colocado na posição correta cada vez que o pop-up é aberto
+        }else if(message.type == "state"){
+            if(container.style.display == 'none')
                 sendResponse("hidden");
             else
                 sendResponse("visible");
